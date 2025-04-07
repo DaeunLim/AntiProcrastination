d9a15a2cbf8cb58f5b329edbb00406026ccd9c6a
mInsOng9 <117246969+mInsOng9@users.noreply.github.com>
2025-04-07 17:50:55 -0400

    Update Header.js
    Add Header component with fixed layout, hamburger menu, and login link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,21 @@
-import React from 'react';
-import './Header.css'; // If you want separate CSS for header and sidebar
+import React, { useEffect } from 'react';
+import './Header.css';
 
-function Header() {
+function Header({ onMenuClick }) {
   return (
-    <div className="header-container">
-      {/* Header Section */}
-      <header className="header">
-        <div className="logo">
-          {/* You can add an actual logo image here */}
-          <img src="path-to-your-logo.png" alt="Logo" className="logo-img" />
-        </div>
-        <h1 className="title">Calendar App</h1>
-      </header>
+    <header>
+      <div className="top_header_bg"></div>
 
-      {/* Sidebar Section */}
-      <div className="sidebar">
-        <ul>
-          <li><a href="#">Tab 1</a></li>
-          <li><a href="#">Tab 2</a></li>
-          <li><a href="#">Tab 3</a></li>
-          <li><a href="#">Tab 4</a></li>
+      <div className="header_container">
+          {/* You can add an actual logo image here */}
+
+        <button id="btn_more" aria-label="Menu" onClick={onMenuClick}></button>
+        <h1 className="title">Calendar App</h1>
+        <ul id="top_nav">
+          <li><a href=""><span>Login</span></a></li>
         </ul>
       </div>
-    </div>
+    </header>
   );
 }
 
