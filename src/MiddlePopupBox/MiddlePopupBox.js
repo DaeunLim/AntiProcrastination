01d886c0a4d6077446561c79c1f0137f30fa5551
Daeun Lim <daeunlim@Daeuns-MacBook-Air.local>
2025-04-20 17:33:19 -0400

    worked on the popup boxes

---
+++ src/MiddlePopupBox/MiddlePopupBox.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import './MiddlePopupBox.css';
+
+const MiddlePopupBox = ({ onAddTask, onClose }) => {
+    const [date, setDate] = useState('');
+    const [task, setTask] = useState('');
+    const [priority, setPriority] = useState('green');
+
+    const handleAddButton = () => {
+        if (date && task) {
+            onAddTask(date, { task, priority});
+            setDate('');
+            setTask('');
+            setPriority('green');
+            onClose();
+        }
+    };
+
+    return (
+        <div className="add-task">
+            <div className="add-task-title">
+                <h3>Add New Task</h3>
+                <input
+                    type="date"
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Task description"
+                    value={task}
+                    onChange={(e) => setTask(e.target.value)}
+                />
+                <select value={priority} onChange={(e) => setPriority(e.target.value)}>
+                    <option value="red">High (Red)</option>
+                    <option value="orange">Medium (Orange)</option>
+                    <option value="green">Low (Green)</option>
+                </select>
+                <button onClick={handleAddButton}>Add</button>
+                <button onClick={onClose} className="close-button">Close</button>
+            </div>
+        </div>
+    )
+}
+
+export default MiddlePopupBox;
